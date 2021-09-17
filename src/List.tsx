66f88333853c9e7285dd4e71f9ecc172df27a5c0
Heath Daniel <mr.heath.daniel@gmail.com>
2021-09-16 20:30:28 -0400

    Add next button to fetch more pokemon

--- src/List.tsx
+++ src/List.tsx
@@ -5,20 +5,33 @@ import { Pokemon } from "./types";
 const List = () => {
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
   const [count, setCount] = useState(0);
+  const [fetchURL, setFetchURL] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [toFetch, setToFetch] = useState(true);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then(response => response.json())
-      .then(json => {
-        const pokemonList: Pokemon[] = json.results;
-        for (let i = 0; i < pokemonList.length; i++) {
-          pokemonList[i].name = pokemonList[i].name.substring(0, 1).toUpperCase() + pokemonList[i].name.substring(1);
-          pokemonList[i].number = count + i + 1;
-        }
-        setPokemon(pokemonList);
-        setCount(count + pokemonList.length);
-      });
-  }, []);
+    function fetchData() {
+      fetch(fetchURL)
+        .then(response => response.json())
+        .then(json => {
+          const pokemonList: Pokemon[] = json.results;
+          for (let i = 0; i < pokemonList.length; i++) {
+            pokemonList[i].name = pokemonList[i].name.substring(0, 1).toUpperCase() + pokemonList[i].name.substring(1);
+            pokemonList[i].number = count + i + 1;
+          }
+          setPokemon([...pokemon, ...pokemonList]);
+          setCount(count + pokemonList.length);
+          setFetchURL(json.next);
+          setToFetch(false);
+        });
+      }
+    if (toFetch)
+      fetchData();
+  }, [toFetch]);
+
+  const handleNext = () => {
+    setToFetch(true);
+  };
+
 
   return (
     <div>
@@ -28,6 +41,9 @@ const List = () => {
           <li key={pokemon.name}>{pokemon.number}. {pokemon.name}</li>
         ))}
       </ul>
+      <button onClick={handleNext}>
+        Next
+      </button>
     </div>
   );
 };
