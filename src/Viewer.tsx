401d5cdd26a0c6839c79825eea4b563decc30a89
Heath Daniel <mr.heath.daniel@gmail.com>
2021-09-24 19:46:05 -0400

    Fix linewrap on viewer, other layout adjustments

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 import { PokemonDisplayDetails } from "./types";
 
-const TextBoxRow = (props: any): JSX.Element => (
+type TextBoxRowProps = {
+  children: JSX.Element | JSX.Element[]
+}
+
+const TextBoxRow = ({ children }: TextBoxRowProps): JSX.Element => (
   <div className="flex justify-between w-full px-5 md:px-3">
-    {props.children}
+    {children}
   </div>
 );
 
@@ -52,18 +56,18 @@ const Viewer = ({ pokemon, isLoading, error }: ViewerProps) => {
 
         {!error && pokemon && (
         <div className="flex w-full px-5 mx-1 bg-red-500 border-4 border-black rounded h-1/3">
-          <div className="flex flex-col items-center justify-center w-full border-t-2 border-b-2 border-l-4 border-r-4 border-red-200 bg-off-white">
+          <div className="flex flex-col items-center justify-between w-full py-2 border-t-2 border-b-2 border-l-4 border-r-4 border-red-200 bg-off-white">
             <TextBoxRow>
               <p>Type:</p>
-              <p>{typeText}</p>
+              <p className="text-right">{typeText}</p>
             </TextBoxRow>
             <TextBoxRow>
               <p>Weight:</p>
-              <p>{weightText}</p>
+              <p className="text-right">{weightText}</p>
             </TextBoxRow>
             <TextBoxRow>
               <p>Height:</p>
-              <p>{heightText}</p>
+              <p className="text-right">{heightText}</p>
             </TextBoxRow>
           </div>
         </div>
